012b413350b4b9fe549c8e67cb349d5fb291e1b9
CS-PRIO30 <cs.prio30@gmail.com>
2017-04-01 21:36:07 +0200

    Update formel_1_v2.py

--- formel_1_v2.py
+++ formel_1_v2.py
@@ -100,7 +100,7 @@ def sendTelegraph( articleImage, articleTitle, articleUrl, string ,feed ):
 	html_content = "<h4><b>" + articleTitle + "</b></h4>" + "<i><u>" + translate(articleTitle,"en","de") + "</u></i>\n" + "<a href=\"" + articleUrl + "\">LINK</a>" + stringAll
 	
 	page = telegraph.createPage( title="Formel-1",  html_content= html_content, author_name="f126ck" )
-	url2send = 'http://telegra.ph/{}'.format(page['path'].encode("utf-8"))
+	url2send = 'http://telegra.ph/{}'.format(page['path'])
 	catIntro = getCategoryIntro( feed )
 	
 	tts = gTTS(text = articleTitle + string, lang='de')
